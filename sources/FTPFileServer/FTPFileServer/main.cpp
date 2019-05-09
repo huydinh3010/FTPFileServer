9b2d4bb16755263b4b41bae8f0db2c97be4111d2
huydinh3010 <huydinh3010@gmail.com>
2019-05-09 21:58:09 +0700

    file size command

--- sources/FTPFileServer/FTPFileServer/main.cpp
+++ sources/FTPFileServer/FTPFileServer/main.cpp
@@ -128,6 +128,20 @@ int findFile(const char * fullpath) {
 	else return 3; // tìm được nhiều hơn 1 file/thư mục
 }
 
+int sizeOfFile(const char * fullpath) {
+	// trả về kích thươc file nếu tìm thấy, trả về 0 nếu không tìm thấy
+	WIN32_FIND_DATAA FDATA;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	hFind = FindFirstFileA(fullpath, &FDATA);
+	if (hFind == INVALID_HANDLE_VALUE) return 0;
+	int size = FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow;
+	if (FindNextFileA(hFind, &FDATA) == 0) {
+		if (FDATA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) return 0; // tìm được 1 thư mục
+		else return size; // tìm được 1 file
+	}
+	else return 0; // tìm được nhiều hơn 1 file/thư mục
+}
+
 bool createListCmdData(const char * fullpath,User * user, char * data) { // tao data cho lenh list
 	int type = findFile(fullpath);
 	if (type == 0) return false; // không tìm thấy file / thư mục
@@ -139,7 +153,7 @@ bool createListCmdData(const char * fullpath,User * user, char * data) { // tao 
 		hFind = FindFirstFileA(tmp, &FDATA);
 		//if (hFind == INVALID_HANDLE_VALUE) return true; // khong tim duoc file dau tien, data rỗng, nếu là thư mục chắc chắn tìm được hai thư mục . và ..
 		char metadata[100] = "";
-		sprintf(metadata, "\t%d\t%s\t%s\t", 1, user->username, user->username);
+		sprintf(metadata, " %d %s %s ", 1, user->username, user->username);
 		do {
 			if (FDATA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 				// la thu muc khác .. và .
@@ -155,7 +169,7 @@ bool createListCmdData(const char * fullpath,User * user, char * data) { // tao 
 					char line[1024] = "";
 					SYSTEMTIME stLocal;
 					FileTimeToSystemTime(&FDATA.ftCreationTime, &stLocal);
-					sprintf(line, "%s%s%d\t%02d %02d %04d\t%s\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
+					sprintf(line, "%s%s%d %02d %02d %04d %s\r\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
 					strcat(data, line);
 				}
 			}
@@ -172,7 +186,7 @@ bool createListCmdData(const char * fullpath,User * user, char * data) { // tao 
 				char line[1024] = "";
 				SYSTEMTIME stLocal;
 				FileTimeToSystemTime(&FDATA.ftCreationTime, &stLocal);
-				sprintf(line, "%s%s%d\t%02d %02d %04d\t%s\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
+				sprintf(line, "%s%s%d %02d %02d %04d %s\r\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
 				strcat(data, line);
 			}
 		} while (FindNextFileA(hFind, &FDATA) != 0);
@@ -184,7 +198,7 @@ bool createListCmdData(const char * fullpath,User * user, char * data) { // tao 
 		hFind = FindFirstFileA(fullpath, &FDATA);
 		//if (hFind == INVALID_HANDLE_VALUE) return false; // khong tim duoc file
 		char metadata[100] = "";
-		sprintf(metadata, "\t%d\t%s\t%s\t", 1, user->username, user->username);
+		sprintf(metadata, " %d %s %s ", 1, user->username, user->username);
 		do {
 			if (FDATA.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 				// la thu muc khác .. và .
@@ -200,7 +214,7 @@ bool createListCmdData(const char * fullpath,User * user, char * data) { // tao 
 					char line[1024] = "";
 					SYSTEMTIME stLocal;
 					FileTimeToSystemTime(&FDATA.ftCreationTime, &stLocal);
-					sprintf(line, "%s%s%d\t%02d %02d %04d\t%s\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
+					sprintf(line, "%s%s%d %02d %02d %04d %s\r\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
 					strcat(data, line);
 				}
 			}
@@ -217,13 +231,15 @@ bool createListCmdData(const char * fullpath,User * user, char * data) { // tao 
 				char line[1024] = "";
 				SYSTEMTIME stLocal;
 				FileTimeToSystemTime(&FDATA.ftCreationTime, &stLocal);
-				sprintf(line, "%s%s%d\t%02d %02d %04d\t%s\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
+				sprintf(line, "%s%s%d %02d %02d %04d %s\r\n", permission, metadata, FDATA.nFileSizeHigh * (MAXDWORD + 1) + FDATA.nFileSizeLow, stLocal.wDay, stLocal.wMonth, stLocal.wYear, FDATA.cFileName);
 				strcat(data, line);
 			}
 		} while (FindNextFileA(hFind, &FDATA) != 0);
 		return true;
 	}
 }
+
+
 
 DWORD WINAPI clientThread(LPVOID p) {
 	SOCKET c = (SOCKET)p;
@@ -239,9 +255,12 @@ DWORD WINAPI clientThread(LPVOID p) {
 	char password[NAME_LENGTH] = "";
 
 	// gửi message
-	char message[] = "Chao may.\n";
+	char message[] = "220 Chao may.\r\n";
 	send(c, message, strlen(message), 0);
-	bool trans_mode = 0; // 0: chưa set, 1: active mode, 2: passive mode
+	
+	int trans_mode = 0; // 0: chưa set, 1: active mode, 2: passive mode
+	int data_type = 0; // 0: ascii (default), 1: binary
+
 	SOCKET sdata = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	SOCKADDR_IN addr_data;
 	addr_data.sin_family = AF_INET;
@@ -254,12 +273,12 @@ DWORD WINAPI clientThread(LPVOID p) {
 		// nhan cau lenh tu client
 		char rbuf[1024];
 		memset(rbuf, 0, 1024);
-		recv(c, rbuf, 1023, 0);
-
+		if(recv(c, rbuf, 1023, 0) <= 0) break; // client ngắt kết nối
+		
 		char cmd[5] = "";
 		char args[1024] = "";
 		if (!splitRequestCommand(rbuf, cmd, args)) {
-			char sbuf[] = "500 Systax error, command unrecognized.\n";
+			char sbuf[] = "500 Systax error, command unrecognized.\r\n";
 			send(c, sbuf, strlen(sbuf), 0);
 			continue;
 		}
@@ -267,7 +286,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 		// xu ly cau lenh
 		if (strcmp(cmd, "USER") == 0) {	// lenh USER<SP><username><\n>
 			if (strlen(args) > NAME_LENGTH) { // username quá dài
-				char sbuf[] = "501 Username too long.\n";
+				char sbuf[] = "501 Username too long.\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 			else {
@@ -275,21 +294,21 @@ DWORD WINAPI clientThread(LPVOID p) {
 				if (strlen(args) == 0) {
 					// nhận được username trống 
 					// gửi mã 501: lỗi cú pháp ở tham số lệnh
-					char sbuf[] = "501 Syntax error!\n";
+					char sbuf[] = "501 Syntax error!\r\n";
 					send(c, sbuf, strlen(sbuf), 0);
 				}
 				else {
 					strcpy(username, args); // lấy username: USER<SP><username>\n
-					char sbuf[] = "331 Password required for ";
+					char sbuf[1024] = "331 Password required for ";
 					strcat(sbuf, username);
-					strcat(sbuf, "\n");
+					strcat(sbuf, "\r\n");
 					send(c, sbuf, strlen(sbuf), 0);
 				}
 			}
 		}
 		else if (strcmp(cmd, "PASS") == 0) {	// lenh PASS<sp><password><\n>
 			if (strlen(args) > NAME_LENGTH) { // password quá dài
-				char sbuf[] = "501 Password too long.\n";
+				char sbuf[] = "501 Password too long.\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 			else if(!login){ // neu chua login thi kiem tra tai khoan mat khau 
@@ -298,7 +317,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 					for (int i = 0; i < users.size(); i++) {
 						if (strcmp(username, users.at(i)->username) == 0 && strcmp("null", users.at(i)->password) == 0) {
 							// pass = null tuong ung moi pass
-							char sbuf[] = "230 Logged on\n";
+							char sbuf[] = "230 Logged on.\r\n";
 							send(c, sbuf, strlen(sbuf), 0);
 							login = true;
 							user = users.at(i);
@@ -313,7 +332,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 						// password = null tuong ung voi moi pass
 						if (strcmp(username, users.at(i)->username) == 0 && (strcmp(password, users.at(i)->password) == 0 || strcmp("null", users.at(i)->password) == 0)) {
 							// neu tim thay thi dang nhap thanh cong
-							char sbuf[] = "230 Logged on\n";
+							char sbuf[] = "230 Logged on.\r\n";
 							send(c, sbuf, strlen(sbuf), 0);
 							login = true;
 							user = users.at(i);
@@ -323,20 +342,20 @@ DWORD WINAPI clientThread(LPVOID p) {
 				}
 				if (!login) { // sau qua trinh kiem tra van khong login duoc
 					// gui ma loi tai khoan hoac mat khau khong chinh xac
-					char sbuf[] = "530 User or Password incorrect!\n";
+					char sbuf[] = "530 User or Password incorrect!.\r\n";
 					send(c, sbuf, strlen(sbuf), 0);
 				}
 			}
 			else { // dang login ma gui PASS
 				// gui ma 503: dang login ma gui command PASS, thi bao loi cu phap
-				char sbuf[] = "503 Bad sequence of command\n";
+				char sbuf[] = "503 Bad sequence of command.\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 		}
 		else if (strcmp(cmd, "PASV") == 0) {
 			if (!login) {
 				// yêu cầu login 
-				char sbuf[] = "530 Please login with USER and PASS!\n";
+				char sbuf[] = "530 Please login with USER and PASS!.\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 			else {
@@ -345,12 +364,16 @@ DWORD WINAPI clientThread(LPVOID p) {
 				while (true) {
 					// lưu cấu trúc địa chỉ
 					addr_data.sin_addr.s_addr = 0;
-					addr_data.sin_port = htons(rand() % (65535 - 1024 + 1) + 1024);	// sinh port ngẫu nhiên trong khoảng từ 1024 -> 65535
-					if (!bind(sdata, (sockaddr*)&addr_data, sizeof(addr_data))) break;	// nếu bind thành công thì thoát, tránh trường hợp port đã được sử dụng bởi tiến trình khác
+					srand(time(NULL));
+					addr_data.sin_port = htons(rand() % (65535 - 50000 + 1) + 50000);	// sinh port ngẫu nhiên trong khoảng từ 1024 -> 65535
+					if (!bind(sdata, (sockaddr*)&addr_data, sizeof(addr_data))) {
+						listen(sdata, 1);
+						break;	// nếu bind thành công thì thoát, tránh trường hợp port đã được sử dụng bởi tiến trình khác
+					}
 				}
 				trans_mode = 2;
 				// tạo câu lệnh passive
-				char sbuf[] = "227 Entering Passive Mode (";
+				char sbuf[1024] = "227 Entering Passive Mode (";
 				char ip[] = IP_SERVER;
 				replace(ip, ip + strlen(ip) + 1, '.', ','); // chuyển dấu . thành ,
 				strcat(sbuf, ip);
@@ -360,19 +383,19 @@ DWORD WINAPI clientThread(LPVOID p) {
 				char port[10] = "";
 				sprintf(port, "%d,%d", p1, p2);
 				strcat(sbuf, port);
-				strcat(sbuf, ")\n");
+				strcat(sbuf, ")\r\n");
 				send(c, sbuf, strlen(sbuf), 0); // gửi câu lệnh
 			}
 		}
 		else if (strcmp(cmd, "PORT") == 0) { // lệnh PORT (ip,port) "PORT 122,11,12,5,45,6\n"
 			if (!login) {
 				// yêu cầu login 
-				char sbuf[] = "530 Please login with USER and PASS!\n";
+				char sbuf[] = "530 Please login with USER and PASS!\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 			else {
 				if (strlen(args) == 0) {
-					char sbuf[] = "501 Syntax error!\n";
+					char sbuf[] = "501 Syntax error!\r\n";
 					send(c, sbuf, strlen(sbuf), 0);
 				}
 				else {
@@ -389,7 +412,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 						//*(index[i]) = '\0';
 					}
 					if (!valid) { // lỗi cú pháp
-						char sbuf[] = "501 Syntax error!\n";
+						char sbuf[] = "501 Syntax error!\r\n";
 						send(c, sbuf, strlen(sbuf), 0);
 					}
 					else {
@@ -404,7 +427,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 						// kiểm tra giá trị ip có hợp lệ không
 						if (ip1 < 0 || ip1 > 255 || ip2 < 0 || ip2 > 255 || ip3 < 0 || ip3 > 255 || ip4 < 0 || ip4 > 255 || p1 < 0 || p1 > 255 || p2 < 0 || p2 > 255) {
 							valid = false;
-							char sbuf[] = "501 Syntax error!\n";
+							char sbuf[] = "501 Syntax error!\r\n";
 							send(c, sbuf, strlen(sbuf), 0);
 						}
 						//else if (ip1 != caddr.sin_addr.S_un.S_un_b.s_b1 || ip2 != caddr.sin_addr.S_un.S_un_b.s_b2 || ip3 != caddr.sin_addr.S_un.S_un_b.s_b3 || ip4 != caddr.sin_addr.S_un.S_un_b.s_b4) {
@@ -425,7 +448,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 							/*cout << ip1 << "." << ip2 << "." << ip3 << "." << ip4 << "." << p1 << "." << p2 << endl;*/
 							/*cout << addr_data.sin_addr.s_addr << endl;*/
 							// gửi phản hồi đến client
-							char sbuf[] = "200 Port command successful!\n";
+							char sbuf[] = "200 Port command successful!\r\n";
 							send(c, sbuf, strlen(sbuf), 0);
 						}
 					}
@@ -505,18 +528,21 @@ DWORD WINAPI clientThread(LPVOID p) {
 		else if (strcmp(cmd, "LIST") == 0) { // xử lý lệnh LIST<sp><path><\n>
 			if(!login){
 				// yêu cầu login 
-				char sbuf[] = "530 Please login with USER and PASS!\n";
+				char sbuf[] = "530 Please login with USER and PASS!\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 			else if (strlen(args) > PATH_LENGTH_MAX) {
-				char sbuf[] = "501 Pathname too long.\n";
+				char sbuf[] = "501 Pathname too long.\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			}
 			else if (trans_mode == 0) {
-				char sbuf[] = "425 Use PASV or PORT command first.\n";
+				char sbuf[] = "425 Use PASV or PORT command first.\r\n";
 				send(c, sbuf, strlen(sbuf), 0);
 			} 
 			else{
+				if (strcmp(args, "-l") == 0) strcpy(args, ""); // 
+				
+				
 				char path[2*PATH_LENGTH_MAX] = "";
 				if (strlen(args) == 0) strcpy(path, cwd); // path bỏ trống, lấy path trong cwd
 				else {
@@ -526,22 +552,17 @@ DWORD WINAPI clientThread(LPVOID p) {
 				// chưa xử lý kí tự đặc biệt như: cd .., cd ., cd ~ giống trong linux
 				
 				preprocessPathname(path);
-				if (strcmp(path, "/") == 0) path[0] = '\0';
 				// ghép thành đường dẫn vật lý
 				char fullpath[2 * PATH_LENGTH_MAX] = "";
 				sprintf(fullpath, "%s%s", user->h_path, path);
 
-				//replace(fullpath, fullpath + strlen(fullpath) + 1, '/','\\'); // thay thế ký tự / thành '\'
-				//// tìm và xử lý trong bộ nhớ
-				
 				char data[1024] = "";
 				if (createListCmdData(fullpath, user, data)) {
-					//data[strlen(data) - 1] = '\0'; // xóa \n ở cuối
 					// mở kết nối mới và gửi dữ liệu đi
 					// gửi mã 150
 					char sbuf[1024] = "150 Opening data channel for directory listing of ";
 					strcat_s(sbuf, args);
-					strcat_s(sbuf, "\n");
+					strcat_s(sbuf, "\r\n");
 					send(c, sbuf, strlen(sbuf), 0);
 					if (trans_mode == 1) {
 						// active mode
@@ -557,7 +578,7 @@ DWORD WINAPI clientThread(LPVOID p) {
 						if (code != 0) {
 							// không kết nối được
 							// gửi code báo lỗi
-							char sbuf[] = "425 Can't open data connection.\n";
+							char sbuf[] = "425 Can't open data connection.\r\n";
 							send(c, sbuf, strlen(sbuf), 0);
 						}
 						else {
@@ -567,13 +588,13 @@ DWORD WINAPI clientThread(LPVOID p) {
 							closesocket(sdata); // đóng kết nối
 							char sbuf[1024] = "226 Successful transfering ";
 							strcat_s(sbuf, args);
-							strcat_s(sbuf, "\n");
+							strcat_s(sbuf, "\r\n");
 							send(c, sbuf, strlen(sbuf), 0);
 						}
 					}
 					else if (trans_mode == 2) {
 						// passive mode
-						listen(sdata, 1);
+						// listen(sdata, 1);
 						SOCKADDR_IN caddr_t;
 						int clen_t = sizeof(caddr_t);
 						SOCKET sd = accept(sdata, (sockaddr*)&caddr_t, &clen_t);
@@ -582,8 +603,9 @@ DWORD WINAPI clientThread(LPVOID p) {
 						send(sd, data, strlen(data), 0);
 						closesocket(sd);
 						char sbuf[1024] = "226 Successful transfering ";
+
 						strcat_s(sbuf, args);
-						strcat_s(sbuf, "\n");
+						strcat_s(sbuf, "\r\n");
 						send(c, sbuf, strlen(sbuf), 0);
 					}
 					trans_mode = 0;
@@ -591,26 +613,71 @@ DWORD WINAPI clientThread(LPVOID p) {
 				else {
 					// không tìm thấy, gửi thông báo lỗi
 					// gửi mã 550 File or Directory not found.
-					char sbuf[] = "550 File or Directory not found.\n";
+					char sbuf[] = "550 File or Directory not found.\r\n";
 					send(c, sbuf, strlen(sbuf), 0);
 				}
 				
 			}
 		}
-
-
-
-		else {	// lenh khong duoc xu ly -> loi cu phap
-			char sbuf[] = "500 Systax error, command unrecognized.\n";
+		else if (strcmp(cmd, "SYST") == 0) { // thông tin về hệ thống
+			char sbuf[] = "215 UNIX\r\n";
 			send(c, sbuf, strlen(sbuf), 0);
 		}
+		else if (strcmp(cmd, "SIZE") == 0) { // size của file, thư mục thì báo lỗi	
+			char path[2 * PATH_LENGTH_MAX] = "";
+			if (strlen(args) == 0) strcpy(path, cwd); // path bỏ trống, lấy path trong cwd
+			else {
+				if (args[0] == '/') strcpy(path, args);// đường dẫn tuyệt đối
+				else sprintf(path, "%s%s", cwd, args); // đường dẫn tương đối
+			}
+			preprocessPathname(path);
+			char fullpath[2 * PATH_LENGTH_MAX] = "";
+			sprintf(fullpath, "%s%s", user->h_path, path);
+			int size = sizeOfFile(fullpath);
+			if (size != 0) {
+				char sbuf[1024] = "";
+				sprintf(sbuf, "%d %d\r\n", 213, size);
+				send(c, sbuf, strlen(sbuf), 0);
+			}
+			else {
+				char sbuf[] = "550 File not found.\r\n";
+				send(c, sbuf, strlen(sbuf), 0);
 
+			}
+		}
+		else if (strcmp(cmd, "TYPE") == 0) { // kiểu dữ liệu biểu diễn
+			if (strcmp(args, "I") == 0) { // kiểu nhị phân
+				data_type = 1;
+				char sbuf[] = "200 Type set to I.\r\n";
+				send(c, sbuf, strlen(sbuf), 0);
+			}
+			else if (strcmp(args, "A") == 0) { // kiểu ascii
+				data_type = 0;
+				char sbuf[] = "200 Type set to A.\r\n";
+				send(c, sbuf, strlen(sbuf), 0);
+			}
+			else if (strcmp(args, "E") == 0 || strcmp(args, "L") == 0) {
+				// không hỗ trợ 2 kiểu này
+				char sbuf[] = "421 Does not support this data type.\r\n";
+				send(c, sbuf, strlen(sbuf), 0);
+			}
+			else {
+				// lỗi tham số
+				char sbuf[] = "501 Syntax error.\r\n";
+				send(c, sbuf, strlen(sbuf), 0);
+			}
+		}
+		else if (strcmp(cmd, "QUIT") == 0) { // kết thúc phiên
+			char sbuf[] = "221 Goodbye.\r\n";
+			send(c, sbuf, strlen(sbuf), 0);
+			break;
+		} 
+		else {	// lenh khong duoc xu ly -> loi cu phap
+			char sbuf[] = "500 Systax error, command unrecognized.\r\n";
+			send(c, sbuf, strlen(sbuf), 0);
+		}
 	}
-
-	
-	
 	return 0;
-
 }
 
 int main() {
